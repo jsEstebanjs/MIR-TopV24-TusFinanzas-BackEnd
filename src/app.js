ed2b28d7f6d4c5f7fc1a119361a136ec658ba947
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-12-06 15:34:20 -0500

    origin *

--- src/app.js
+++ src/app.js
@@ -11,7 +11,7 @@ const cors = require('cors')
 const app = express();
 app.use(express.json())
 app.use(cors({
-  "origin": `${process.env.ORIGIN}`,
+  "origin": `*`,
   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
   "preflightContinue": false,
   "optionsSuccessStatus": 204
