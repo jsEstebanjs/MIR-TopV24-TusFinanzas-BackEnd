c8c61e70edfcd881edb5d6a049d5d6915e4f549f
Esteban Carreño <juanestebancarrenocamelo@gmail.com>
2022-12-06 15:27:12 -0500

    fix

--- src/app.js
+++ src/app.js
@@ -8,18 +8,19 @@ const subcategoriesRouter = require('./api/subCategories/subCategories.route')
 const mediaRoute = require('./api/multimedia/media.route')
 const cors = require('cors')
 
+
 const app = express();
 app.use(express.json())
 app.use(cors({
-    "origin": `${process.env.ORIGIN}`,
-    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
-    "preflightContinue": false,
-    "optionsSuccessStatus": 204
-  }))
-app.use('/users',userRouter)
-app.use('/transactions',transactionsRouter)
-app.use('/categories',categoriesRouter)
-app.use('/subcategories',subcategoriesRouter)
+  "origin": `${process.env.ORIGIN}`,
+  "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  "preflightContinue": false,
+  "optionsSuccessStatus": 204
+}))
+app.use('/users', userRouter)
+app.use('/transactions', transactionsRouter)
+app.use('/categories', categoriesRouter)
+app.use('/subcategories', subcategoriesRouter)
 app.use(mediaRoute)
 app.use(morgan("dev"))
 
